9830ea4591af2069557aa60e1b830dc620cb9a59
Eduardo Fernandez <yo@edufdezsoy.es>
2020-10-12 17:00:45 +0200

    basic table pushing this to let yeza work

--- assets/js/rss-presenter.js
+++ assets/js/rss-presenter.js
@@ -1,14 +1,23 @@
 class RssPresenter {
     constructor(data) {
-        this.data = data;
+        this.chapters = data;
 
-        console.log(this.data)
-        document.getElementById("debug").innerHTML = "";
+        console.log(this.chapters)
+        document.getElementById("release-table").innerHTML = "";
+        document.getElementById("release-table").classList.remove("is-hidden");
+        document.getElementById("loading").classList.add("is-hidden");
 
-        const items = data.querySelectorAll("item");
-
-        items.forEach(element => {
-            document.getElementById("debug").innerHTML += element.querySelector("title").innerHTML + "<br>"
+        this.chapters.forEach(element => {
+            document.getElementById("release-table").innerHTML += `
+            <tr>
+                <td><a href="${element.site_url}">${element.anime}</a></td>
+                <td>${element.chapter}</td>
+                <td data-action="collapse" data-target="#test">torrent</td>
+                <td>magnet
+            <td>
+            </tr>
+            `;
+            
         });
     }
 }
